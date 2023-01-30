73b175aeab3ba81affeffe7c6976d38464f88f27
Aurelio Marín Bautista <72474524+vKurama7u7v@users.noreply.github.com>
2023-01-29 18:03:33 -0600

    add limit graphql

--- apollo/gql/tags.gql.js
+++ apollo/gql/tags.gql.js
@@ -2,7 +2,7 @@ import { gql } from "@apollo/client";
 
 export const GET_TAGS = gql`
   query GetTags {
-    tags {
+    tags(sort: "name:asc", pagination: { limit: 100 }) {
       data {
         id
         attributes {
